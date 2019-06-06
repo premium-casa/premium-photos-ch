1c535763b495e5997198be4edf1f13b75ca0822a
Chris Huang <chrishuang128@gmail.com>
2019-06-06 14:08:09 -0700

    added new script for data geneartion

--- data-generation/data-generation-noSql.js
+++ data-generation/data-generation-noSql.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const faker = require('faker');
-
+// const randomNum = (min, max) =>
+// 	Math.floor(Math.random() * (max - min + 1) + min);
 // write.write('id, issaved, listingdesc, villa, kitchen, livingroom, bed1, bath1, lobby, bed2, office, bath2, bed3\n');
 // for (let i = 0; i < 10000000; i++) {
 // 	write.write((i+1) + ',' + faker.random.boolean() + ',' + faker.lorem.sentence() + ',' + faker.image.imageUrl() + ',' +
@@ -14,36 +15,50 @@ const faker = require('faker');
 // 	faker.image.imageUrl() + ',' +
 // 	faker.image.imageUrl() + '\n');
 // }
+
 async function dataGenerate() {
 	const write = fs.createWriteStream('../chrisdata.csv', {
 		encoding: 'utf8',
 		flag: 'a'
 	});
 
-	const rooms = [
-		'villa',
-		'kitchen',
-		'livingroom',
-		'bed1',
-		'bath1',
-		'lobby',
-		'bed2',
-		'office',
-		'bath2',
-		'bed3'
-	];
-	write.write('id, issaved, url, listingdesc, listing_id, room\n');
-	for (let i = 1; i <= 10; i++) {
-		for (let x = 0; x < 10; x++) {
+const rooms = [
+	'villa',
+	'kitchen',
+	'livingroom',
+	'bed1',
+	'bath1',
+	'lobby',
+	'bed2',
+	'office',
+	'bath2',
+	'bed3'
+];
+
+const roomGenerate = function() {
+	let result = '';
+	for (let i = 0; i < rooms.length; i++) {
+		if (i !== rooms.length - 1) {
+			result +=
+				rooms[i] + ': ' + faker.image.imageUrl(1920, 1080, 'city') + ', ';
+		} else {
+			result += rooms[i] + ': ' + faker.image.imageUrl(1920, 1080, 'city');
+		}
+	}
+	return result;
+};
+
+	write.write('id, issaved, listingdesc, photos\n');
+	for (let i = 1; i <= 10000000; i++) {
 			const ableToWrite = write.write(
-				((i-1) * 10 + x + 1) + ',' + false + ',' + faker.image.imageUrl(1920, 1080, 'city') + ',' + faker.lorem.sentence() + ',' + i + ',' + rooms[x] + '\n'
+				(i+1) + '|' + false + '|' + faker.lorem.sentence() + '|' + '{' + roomGenerate() + '}' + '\n'
 			);
 			if (!ableToWrite) {
 				await new Promise((resolve) => {
 					write.once('drain', resolve);
 				});
 			}
-		}
+
 	}
 
 	write.end();
@@ -51,3 +66,4 @@ async function dataGenerate() {
 }
 
 dataGenerate();
+
